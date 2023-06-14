8d6d78458bc7eb1cd6fbe85105e51b3d92f60213
Aaquibzone <codewithaaquib@gmail.com>
2023-06-14 09:47:55 +0530

    WAP to print square of natural numbers upto given number using recursion

---
+++ src/main/java/com/bootcoding/java/Recursion/RecursionEx4.java
@@ -0,0 +1,28 @@
+package com.bootcoding.java.Recursion;
+
+public class RecursionEx4 {
+//WAP to print square of natural numbers upto given number using recursion
+
+    public static void main(String[] args) {
+        squareOfNaturalNo(5);
+
+    }
+    public static  void squareOfNaturalNo(int n)
+    {
+        if (n==0)
+        {
+            return ;
+        }
+
+         squareOfNaturalNo(n-1);
+    if(n>0){
+        int  k=n*n;
+        System.out.println(n + "is a square of (" +k +")");
+    }
+
+        }
+
+
+
+
+}
