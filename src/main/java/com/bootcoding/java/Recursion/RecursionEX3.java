3b1a46344439405ae6a53d26d6b58228abd8bc90
Aaquibzone <codewithaaquib@gmail.com>
2023-06-14 09:47:26 +0530

    Grade Service of Students

---
+++ src/main/java/com/bootcoding/java/Recursion/RecursionEX3.java
@@ -0,0 +1,27 @@
+package com.bootcoding.java.Recursion;
+
+public class RecursionEX3 {
+    public static void main(String[] args) {
+
+        int res =printFactorial(4);
+        System.out.println("the factorial of  give no is " + res) ;
+   }
+    public static int printFactorial(int n) {
+
+//        if(n<=1)
+//        {
+//            return 1 ;
+//        }
+//        else {
+//            return n*printFactorial(n-1);
+//        }
+
+        if(n==1) {
+            return 1;
+        }
+        return n*printFactorial(n-1);
+        }
+
+
+    }
+
