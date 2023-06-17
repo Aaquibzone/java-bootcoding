2ce7038b731cf3b44362e9bd1840d7516837e7c2
Aaquibzone <codewithaaquib@gmail.com>
2023-06-17 23:34:56 +0530

    MIN & MAX Element of Array

---
+++ src/main/java/oops/MinMaxArray.java
@@ -0,0 +1,32 @@
+//package oops;
+//
+//public class MinMaxArray {
+//
+//    public static void main(String [] args)
+//    {
+//        int [] a={5,8,33,10,2};
+//
+//        int min=0;
+//        int premin =0;
+//        int size= a.length;
+//
+//
+//        for(int i=0;i<size;i++ )
+//        {
+//            min=a[i];
+//
+//            if (a[i] < min) {
+//                min=a[i];
+//               premin =min;
+//
+//                System.out.println(min );
+//            }
+//
+//
+//
+//
+//
+//        }
+//
+//    }
+//}
