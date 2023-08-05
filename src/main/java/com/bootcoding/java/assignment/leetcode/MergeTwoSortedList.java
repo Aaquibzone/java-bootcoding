872a69932b48da113e531dc48be92a120b387ef3
Aaquibzone <codewithaaquib@gmail.com>
2023-08-05 23:49:12 +0530

    MergeTwoSortedList

---
+++ src/main/java/com/bootcoding/java/assignment/leetcode/MergeTwoSortedList.java
@@ -0,0 +1,22 @@
+package com.bootcoding.java.assignment.leetcode;
+
+public class MergeTwoSortedList {
+    public void merge(int A[] , int m, int B[], int n) {
+
+        while(m > 0 && n > 0){
+            if(A[m-1] > B[n-1]){
+                A[m+n-1] = A[m-1];
+                m--;
+
+            }else{
+                A[m+n-1] = B[n-1];
+                n--;
+            }
+        }
+
+        while(n > 0){
+            A[m+n-1] = B[n-1];
+            n--;
+        }
+    }
+}
