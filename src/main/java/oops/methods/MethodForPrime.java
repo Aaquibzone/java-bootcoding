b676e0b771bcf24ac8636400d7d30b771b0e1682
Aaquibzone <codewithaaquib@gmail.com>
2023-08-31 00:43:28 +0530

    MethodForPrime

---
+++ src/main/java/oops/methods/MethodForPrime.java
@@ -0,0 +1,20 @@
+package oops.methods;
+
+// int num = 33, flag = 0;
+//         for(int i=2 ; i < num/2 ; i++) {
+//        if(num%i == 0) {
+//        printf("%d is not a prime number", num);
+//        flag = 1;
+//        break;
+//        }
+//        }
+//        if(flag == 0) {
+//        printf("%d is a prime number", num);
+//        }
+
+public class MethodForPrime {
+    public static void main(String[] args) {
+
+    }
+}
+
