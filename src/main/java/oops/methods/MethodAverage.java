e795d0a0ab1061fa0204ca4f9778e2a0b471363e
Aaquibzone <codewithaaquib@gmail.com>
2023-09-16 22:36:17 +0530

    MethodAverage

---
+++ src/main/java/oops/methods/MethodAverage.java
@@ -0,0 +1,21 @@
+package oops.methods;
+
+public class MethodAverage {
+
+    public static void  average(int a,int b, int c){
+         int average= (a+b+c)/3;
+        System.out.println(average);
+
+
+    }
+
+    public static void main(String[] args) {
+
+        average(25,45,65);
+
+
+
+
+
+    }
+}
---
+++ src/main/java/oops/methods/MethodEx.java
@@ -0,0 +1,13 @@
+
+public class MethodEx {
+
+public static void middleString(){
+    String s="350";
+    System.out.println(s.charAt(1));
+}
+
+    public static void main(String[] args) {
+        middleString();
+
+    }
+}
