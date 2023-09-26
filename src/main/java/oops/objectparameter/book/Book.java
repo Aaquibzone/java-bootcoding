ed86048295de5bb5bb6e8ac5df334a45edad5d53
Aaquibzone <codewithaaquib@gmail.com>
2023-09-26 23:06:51 +0530

    book

---
+++ src/main/java/oops/objectparameter/book/Book.java
@@ -0,0 +1,42 @@
+package oops.objectparameter.book;
+
+public class Book {
+    double price;
+    String bookName;
+
+    String authorName;
+
+    int noOfPage;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public int getNoOfPage() {
+        return noOfPage;
+    }
+
+    public void setNoOfPage(int noOfPage) {
+        this.noOfPage = noOfPage;
+    }
+}
