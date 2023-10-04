fc7fbe9d2184a52273a0292894c416b9095e200e
Aaquibzone <codewithaaquib@gmail.com>
2023-10-04 22:55:27 +0530

    DriveCarApp

---
+++ src/main/java/oops/polymorphism/overloading/driveCar/DriveCarApp.java
@@ -0,0 +1,9 @@
+package oops.polymorphism.overloading.driveCar;
+
+public class DriveCarApp {
+    public static void main(String[] args) {
+     DriveCarService dc= new  DriveCarService();
+     dc.driveCar();
+     dc.print();
+    }
+}
