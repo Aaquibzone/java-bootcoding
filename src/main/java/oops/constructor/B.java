fa1d7dcbabfc29bc8d2c489f8958231222072c5b
Aaquibzone <codewithaaquib@gmail.com>
2023-10-10 14:24:45 +0530

    class B extends A

---
+++ src/main/java/oops/constructor/B.java
@@ -0,0 +1,14 @@
+package oops.constructor;
+
+public class B extends A {
+    public B() {
+
+        System.out.println("Constructor of B");
+    }
+
+    public static void main(String[] args) {
+        B b= new B();
+
+
+    }
+}
