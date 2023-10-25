990236fcf773412758ec91a8c7af421689562ecd
Aaquibzone <codewithaaquib@gmail.com>
2023-10-25 23:46:36 +0530

    PrintService

---
+++ src/main/java/oops/armstrong/PrintService.java
@@ -0,0 +1,21 @@
+//package oops.armstrong;
+//
+//public class PrintService {
+//
+//
+//        public void printingMethod(int result [])
+//        {
+//            for(int i=0;i<result.length;i++)
+//            {
+//                if(result[i]==1)
+//                {
+//                    System.out.println(true);
+//                }
+//                if(result[i]==0)
+//                {
+//                    System.out.println(false);
+//                }
+//            }
+//        }
+//
+//    }
